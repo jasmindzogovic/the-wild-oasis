13fce4a9ed610b9df26f91b694015a60042a0744
Jasmin Dzogovic <jasminxhogovic@gmail.com>
2023-11-10 12:32:25 -0500

    added initial backend for the project

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://cepcwrqtujmxzrgzoksp.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNlcGN3cnF0dWpteHpyZ3pva3NwIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTk2MzI3NjEsImV4cCI6MjAxNTIwODc2MX0.kdigLjqg0keoCbTfDy8sEiLyGjvr5MV8yBVcUVYAeJA";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
