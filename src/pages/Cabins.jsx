1ee7162551b17223ba9f13b6ebd2688e432f7087
Jasmin Dzogovic <jasminxhogovic@gmail.com>
2023-11-14 13:13:06 -0500

    implemented cabin adding functionality

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,10 +1,15 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
+import Button from "../ui/Button";
 
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import CabinTable from "../features/cabins/CabinTable";
+import CreateCabinForm from "../features/cabins/CreateCabinForm";
 
 function Cabins() {
+  const [showForm, setShowForm] = useState(false);
+
   return (
     <>
       <Row type="horizontal">
@@ -13,7 +18,12 @@ function Cabins() {
       </Row>
 
       <Row>
-        <CabinTable></CabinTable>
+        <CabinTable />
+
+        <Button onClick={() => setShowForm((show) => !show)}>
+          {showForm ? "Close Form" : "Add new cabin"}
+        </Button>
+        {showForm && <CreateCabinForm />}
       </Row>
     </>
   );
