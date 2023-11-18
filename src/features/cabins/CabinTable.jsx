4cd50111ef17a6644992b2786216bb69ed679b13
Jasmin Dzogovic <jasminxhogovic@gmail.com>
2023-11-18 11:29:09 -0500

    built the initial scaffold for bookings table

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -1,19 +1,22 @@
+import { useSearchParams } from "react-router-dom";
+
+import { useCabins } from "./useCabins";
+
 import Spinner from "../../ui/Spinner";
 import CabinRow from "./CabinRow";
-import { useCabins } from "./useCabins";
 import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
-import { useSearchParams } from "react-router-dom";
+import Empty from "../../ui/Empty";
 
 function CabinTable() {
   const { isLoading, cabins } = useCabins();
   const [searchParams] = useSearchParams();
 
   if (isLoading) return <Spinner />;
-
-  const filterValue = searchParams.get("discount") || "all";
+  if (cabins.length === 0) return <Empty resource="cabins" />;
 
   // Filter
+  const filterValue = searchParams.get("discount") || "all";
 
   let filteredCabins;
 
