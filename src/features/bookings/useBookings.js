1bb840d7f1c493a7a14321fec42f77185b2e917d
Jasmin Dzogovic <jasminxhogovic@gmail.com>
2023-11-19 11:48:11 -0500

    implemented booking filter and sorting

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,10 +1,29 @@
 import { useQuery } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
+import { useSearchParams } from "react-router-dom";
 
 export function useBookings() {
+  const [searchParams] = useSearchParams();
+
+  // Filter
+  const filteredValue = searchParams.get("status");
+  const filter =
+    !filteredValue || filteredValue === "all"
+      ? null
+      : { field: "status", value: filteredValue };
+
+  // Sort
+  const sortByRaw = searchParams.get("sortBy") || "startDate-desc";
+  const [field, direction] = sortByRaw.split("-");
+  const sortBy = {
+    field,
+    direction,
+    method: "order",
+  };
+
   const { isLoading, data: bookings } = useQuery({
-    queryKey: ["bookings"],
-    queryFn: getBookings,
+    queryKey: ["bookings", filter, sortBy],
+    queryFn: () => getBookings({ filter, sortBy }),
   });
 
   return { isLoading, bookings };
