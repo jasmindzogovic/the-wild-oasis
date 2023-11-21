9a2262f112c8cde5b33d6a112bedfb1a384f6208
Jasmin Dzogovic <jasminxhogovic@gmail.com>
2023-11-21 17:49:59 -0500

    implemented authentication functionality for app

---
+++ src/features/authentication/useLogin.js
@@ -0,0 +1,21 @@
+import { useMutation } from "@tanstack/react-query";
+import { login as loginApi } from "../../services/apiAuth";
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+
+export function useLogin() {
+  const navigate = useNavigate();
+
+  const { isLoading, mutate: login } = useMutation({
+    mutationFn: ({ email, password }) => loginApi({ email, password }),
+    onSuccess: (user) => {
+      navigate("/dashboard");
+    },
+    onError: (error) => {
+      console.log("Error", error);
+      toast.error("Provided email or password are incorrect");
+    },
+  });
+
+  return { isLoading, login };
+}
