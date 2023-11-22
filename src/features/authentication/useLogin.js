fb5195011acb7bc737c6895ca0adbf190343cfe2
Jasmin Dzogovic <jasminxhogovic@gmail.com>
2023-11-22 15:35:55 -0500

    fixed minor bug

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -10,7 +10,7 @@ export function useLogin() {
   const { isLoading, mutate: login } = useMutation({
     mutationFn: ({ email, password }) => loginApi({ email, password }),
     onSuccess: (user) => {
-      queryClient.setQueriesData({ queryKey: ["user"], user });
+      queryClient.setQueryData(["user"], user.user);
       navigate("/dashboard", { replace: true });
     },
     onError: (error) => {
