290b9692e1cb5981573ec41363d196d2e0a2ca08
Jasmin Dzogovic <jasminxhogovic@gmail.com>
2023-11-22 14:56:30 -0500

    implemented authorization route protection

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -10,3 +10,17 @@ export async function login({ email, password }) {
 
   return data;
 }
+
+export async function getUser() {
+  const { data: session } = await supabase.auth.getSession();
+
+  if (!session.session) return null;
+
+  const { data, error } = await supabase.auth.getUser();
+
+  console.log(data);
+
+  if (error) throw new Error(error.message);
+
+  return data?.user;
+}
