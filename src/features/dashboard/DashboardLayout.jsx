9022515324239f9d7bed6d3776723c4f18c74424
Jasmin Dzogovic <jasminxhogovic@gmail.com>
2023-11-23 16:03:48 -0500

    computed recent bookings and stays

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import { useRecentBookings } from "./useRecentBookings";
+import Spinner from "../../ui/Spinner";
+import { useRecentStays } from "./useRecentStays";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -8,6 +11,15 @@ const StyledDashboardLayout = styled.div`
 `;
 
 function DashboardLayout() {
+  const { data, isLoading } = useRecentBookings();
+  const {
+    isLoading: isLoadingStays,
+    data: stays,
+    confirmedStays,
+  } = useRecentStays();
+
+  if (isLoading || isLoadingStays) return <Spinner />;
+
   return (
     <StyledDashboardLayout>
       <div>Statistics</div>
---
+++ src/features/dashboard/useRecentStays.js
@@ -0,0 +1,25 @@
+import { useSearchParams } from "react-router-dom";
+import { getStaysAfterDate } from "../../services/apiBookings";
+import { subDays } from "date-fns";
+import { useQuery } from "@tanstack/react-query";
+
+export function useRecentStays() {
+  const [searchParams] = useSearchParams();
+
+  const numDays = !searchParams.get("last")
+    ? 7
+    : Number(searchParams.get("last"));
+
+  const queryDate = subDays(new Date(), numDays).toISOString();
+
+  const { isLoading, data } = useQuery({
+    queryKey: ["stays", `last-${numDays}`],
+    queryFn: () => getStaysAfterDate(queryDate),
+  });
+
+  const confirmedStays = data?.filter(
+    (stay) => stay.status === "checked-in" || "checked-out"
+  );
+
+  return { data, isLoading, confirmedStays };
+}
